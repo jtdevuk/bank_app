1edc82c61f07f4e3c467873720ca32ac46a47769
John Tweeddale <foureggs@hotmail.com>
2020-05-03 19:37:54 +0100

    Add edit user page and styling

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,15 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
 end
+
+
+# params.require(:person).permit(:name, :age)
