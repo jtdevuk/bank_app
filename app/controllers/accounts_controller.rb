0341cfbce8730ce203456d80dec9a4ae9244df34
John Tweeddale <foureggs@hotmail.com>
2020-05-13 20:09:00 +0100

    Add account limit and delete ability

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,13 +7,25 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(params.require(:account).permit(:account_name, :account_balance))
     @account.customer_id = current_user.id
+
+    if current_user.accounts.count >= 2
+      flash[:notice] = "Customers can only have two accounts"
+      redirect_to current_user and return
+    end
+
     if @account.save
       flash[:notice] = "Account created successfully"
-      redirect_to root_path
+      redirect_to current_user
     else
       flash[:notice] = "Something went wrong"
-      redirect_to root_path
+      redirect_to current_user
     end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to current_user
   end
 
 end
